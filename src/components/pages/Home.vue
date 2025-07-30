8ebdf67165f76476d130f9905d7c5ac6fc4834fd
maks <nazwiskomaks29@gmail.com>
2025-07-30 21:04:39 +0200

    update

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,7 +1,7 @@
-<template>
-  <h1>Home</h1>
-</template>
-
 <script></script>
 
-<style lang="scss" scoped></style>
+<template></template>
+
+<style lang="scss" scoped>
+@use "../../styles/base.scss" as *;
+</style>
