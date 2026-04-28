05cdd53d517d2ccb4d1f3bede573459623e65801
maks <nazwiskomaks29@gmail.com>
2026-04-28 20:00:45 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import InfoCard from "./components/pages/InfoCard.vue";
 </script>
 
 <template>
-  <!-- <RouterView /> -->
-  <InfoCard />
+  <RouterView />
+  <!-- <InfoCard /> -->
 </template>
 
 <style scoped lang="scss"></style>
