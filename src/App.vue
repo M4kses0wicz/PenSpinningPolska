ef394371ffec609d3d2836277e0d3e5c8301581a
maks <nazwiskomaks29@gmail.com>
2026-05-18 19:15:07 +0200

    add lenis dependency and implement smooth scrolling in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup>
 import InfoCard from "./components/pages/InfoCard.vue";
+import Lenis from "lenis";
+
+const lenis = new Lenis({
+  duration: 1.3,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smoothTouch: false,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
 </script>
 
 <template>
-  <!-- <RouterView /> -->
-  <InfoCard />
+  <RouterView />
+  <!-- <InfoCard /> -->
 </template>
 
 <style scoped lang="scss"></style>
