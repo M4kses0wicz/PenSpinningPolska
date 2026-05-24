fd39e997970c8116eb7679b3504356c0e3f8fd12
maks <nazwiskomaks29@gmail.com>
2026-05-24 20:44:13 +0200

    Enhance Navbar component with scroll detection and social media links; update styles and variables

--- src/components/UI/Navbar.vue
+++ src/components/UI/Navbar.vue
@@ -1,7 +1,26 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+
+const scrollingDown = ref(false);
+let lastScrollY = 0;
+
+function handleScroll() {
+  const currentY = window.scrollY;
+  scrollingDown.value = currentY > lastScrollY;
+  lastScrollY = currentY;
+}
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+</script>
 
 <template>
-  <nav>
+  <nav :class="{ 'scrolled-down': scrollingDown }">
     <div class="wrapper">
       <div class="navitem">
         <p>
@@ -29,15 +48,34 @@
 
     <div class="bottom-content-wrapper">
       <div class="socials">
-        <i class="fa-solid fa-user"></i>
-        <i class="fa-brands fa-github"></i>
-        <i class="fa-solid fa-house"></i>
+        <a
+          href="https://www.facebook.com/groups/penspinningpl"
+          target="_blank"
+          title="Nasza grupa na Facebooku"
+        >
+          <i class="fa-brands fa-facebook"></i>
+        </a>
+
+        <a
+          href="https://discord.gg/MCXEnkh"
+          target="_blank"
+          title="Dołącz do naszego Discorda"
+        >
+          <i class="fa-brands fa-discord"></i>
+        </a>
+
+        <a
+          href="https://www.youtube.com/channel/UCX5b5jV6V5Q5Q5Q5Q5Q5Q5Q"
+          target="_blank"
+          title="Zobacz nasz kanał na YouTube"
+        >
+          <i class="fa-brands fa-youtube"></i>
+        </a>
       </div>
       <div class="copy">
         <p>
-          Strona stworzona przez: <br /><a
-            href="https://klemensowicz.pl"
-            target="_blank"
+          Strona stworzona przez: <br />
+          <a href="https://klemensowicz.pl" target="_blank" title="Developer"
             >Klemensowicz - M4k</a
           >
         </p>
@@ -63,6 +101,7 @@ nav {
   flex-direction: column;
   justify-content: space-between;
   height: 100svh;
+  mix-blend-mode: difference;
 
   .wrapper {
     display: flex;
@@ -71,7 +110,6 @@ nav {
     .navitem {
       margin-top: 25px;
       width: 20%;
-      cursor: pointer;
 
       p {
         font-size: $font-size-subheading;
@@ -85,11 +123,18 @@ nav {
       dt {
         font-size: $font-size-subheading;
         font-weight: bold;
+        cursor: pointer;
       }
+
       dd {
         font-size: $font-size-body;
-        opacity: 0.75;
         max-width: 300px;
+        opacity: 0.75;
+        transform: translateY(0px);
+        transition:
+          opacity 0.5s ease,
+          transform 0.4s ease;
+        cursor: default;
       }
     }
   }
@@ -101,14 +146,40 @@ nav {
 
     .socials {
       display: flex;
-      gap: 20px;
+      flex-direction: column;
+      gap: 50px;
+      font-size: $font-size-title;
+
+      i {
+        transition: 0.25s ease;
+      }
+
+      i:hover {
+        color: $green;
+      }
     }
 
     .copy {
       font-size: $font-size-small;
-      opacity: 0.75;
+      opacity: 0.5;
       text-align: right;
+      align-self: flex-end;
+
+      a {
+        font-size: calc($font-size-small * 0.9);
+      }
+
+      a:hover {
+        text-decoration: none;
+      }
     }
+  }
+}
+
+nav.scrolled-down {
+  .wrapper .navitem dd {
+    opacity: 0;
+    // transform: translateY(-8px);
   }
 }
 </style>
