97612fae926c0b26849bb75fe56b93f03627b25d
maks <nazwiskomaks29@gmail.com>
2026-05-25 21:28:21 +0200

    Refactor Home component layout and styles; add main section with logo

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -6,8 +6,25 @@ import Navbar from "../UI/Navbar.vue";
 <template>
   <Navbar />
   <BackgroundLines />
+  <main>
+    <div class="logo"></div>
+  </main>
 </template>
 
 <style lang="scss" scoped>
 @use "../../styles/base.scss" as *;
+main {
+  width: 100vw;
+  height: 100svh;
+  @include FlexCenter;
+
+  .logo {
+    aspect-ratio: 1/1;
+    height: 70%;
+    background-image: url("../../assets/ppp-raczka-white.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+}
 </style>
